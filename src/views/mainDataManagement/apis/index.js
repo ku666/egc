afff621851f408b872a5d71483d04789c9dd1dde
qiyongping <qiyongping@hdsc.com>
2018-01-02 10:41:02 +0800

    delete mainDataManagement

--- src/views/mainDataManagement/apis/index.js
+++
@@ -1,8 +0,0 @@
-import Axios from '@/assets/js/AxiosPlugin'
-
-// 接口地址
-const BASE_PATH = '/scp-modelmgmtcomponent'
-
-// 获取模型信息
-export const getModelList = params => { return Axios.get(BASE_PATH + '/model/list', { params: params }).then(res => res.data) }
-export const requestLogin = params => { return Axios.post(BASE_PATH + '/user/login', { params: params }).then(res => res.data) }
