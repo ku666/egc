8206386c3a41d00dd8f7461965ca78c41dd7200f
dengdaichao <dengdaichao@hdsc.con>
2018-01-19 18:26:22 +0800

    解决冲突

--- config/index.js
+++ config/index.js
@@ -16,27 +16,16 @@ module.exports = {
       '/egc-mapanalysiscomponent': {
         target: 'http://172.16.30.27:8082/' // http://192.168.0.186:38761/
       },
-<<<<<<< HEAD
-=======
       // '/egc-smartcommunitymgmtapp': {
       '/mapAnalysis': {
         // target: 'http://172.16.30.64:8082',// 地图分析
         target: 'http://192.168.0.186:9030',
         changeOrigin: true
       },
->>>>>>> mydev
       '/device': {
         target: 'http://192.168.0.186:9030', // 设备报表
         changeOrigin: true
-      },
-      '/egc-smartcommunitymgmtapp': {
-        target: 'http://172.16.30.64:8082/', // 邓代超（地图分析）
-        changeOrigin: true
       }
-      // '/egc-smartcommunitymgmtapp': {
-      //   target: 'http://172.16.30.64:8082/', // 邓代超（地图分析）
-      //   changeOrigin: true
-      // }
     },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
