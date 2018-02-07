2c464d30cd32d9daef82891163c10fc3028489a6
wuchun <wuchun@hdsc.com>
2018-02-07 11:55:31 +0800

    更新升级 修改配置管理模块  针对服务器硬件 网络设备 应用服务新增导入功能 以及模板导出功能

--- src/views/ApplicationUpgrade/ConfigurationMgmt/NetDeviceSystemInfo.vue
+++ src/views/ApplicationUpgrade/ConfigurationMgmt/NetDeviceSystemInfo.vue
@@ -14,16 +14,12 @@
             <el-table-column fixed="right" label="操作" width="200">
               <template slot-scope="scope">
                 <el-button @click="_handleCheckDetails(scope.$index)" type="text" class="el-icon-view" style="font-size:15px;color: #0078f4" :title="detailsTitle">
-                  <!-- <img :src="details"/> -->
                 </el-button>
                 <el-button @click="_handleEdit(scope.$index)" type="text" class="el-icon-edit"  style="font-size:15px;color: #0078f4" :title="editTitle">
-                  <!-- <img :src="edit" /> -->
                 </el-button>
                 <el-button @click="_handleSynData(scope.$index)" type="text" class="el-icon-refresh" style="font-size:15px;color: #0078f4" :title="refreshTitle">
-                  <!-- <img :src="refresh"/> -->
                 </el-button>
                 <el-button @click="_handleCheckHistory(scope.$index)" type="text" class="el-icon-time" style="font-size:15px;color: #0078f4" :title="historyTitle">
-                  <!-- <img :src="history"/> -->
                 </el-button>
               </template>
             </el-table-column>
@@ -44,21 +40,18 @@
         </div>
       </el-col>
     </el-row>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogDetailsVisible" top="8vh">
-        <net-device-details :netDeviceDetails="netDeviceDetails"></net-device-details >
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogEditVisible" top="8vh">
-        <net-device-edit :netDeviceDetails="netDeviceDetails" @saveOsInfoEvent="_updateOsInfo"></net-device-edit>
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogHistoryVisible" top="8vh" width="80%">
-        <net-device-history :netDeviceHistoryData="netDeviceHistoryData"></net-device-history>
-      </el-dialog>
-    </div>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogDetailsVisible" top="8vh">
+      <net-device-details :netDeviceDetails="netDeviceDetails"></net-device-details >
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogEditVisible" top="8vh">
+      <net-device-edit :netDeviceDetails="netDeviceDetails" @saveOsInfoEvent="_updateOsInfo"></net-device-edit>
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogHistoryVisible" top="8vh" width="80%">
+      <net-device-history :netDeviceHistoryData="netDeviceHistoryData"></net-device-history>
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogUploadVisible" width="40%" :before-close="_handleBeforClose">
+      <config-file-upload ref="uploadCmpt" :uploadFlag="uploadFlag" @handleUploadSuccessEvent="_handleCloseUploadDialog"></config-file-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -67,14 +60,23 @@ import searchCondition from './components/SearchCondition'
 import netDeviceDetails from './components/NetDeviceDetails'
 import netDeviceEdit from './components/NetDeviceEdit'
 import netDeviceHistory from './components/NetDeviceHistory'
-
-import { getNetDeviceInfoByPage, getNetDeviceDetails, updateNetDeviceInfo, getauServersHistoryList, syncNetDeviceData } from './apis/index'
+import ConfigFileUpload from './components/ConfigFileUpload'
+import {
+  getNetDeviceInfoByPage,
+  getNetDeviceDetails,
+  updateNetDeviceInfo,
+  getauServersHistoryList,
+  syncNetDeviceData,
+  downloadResultFile,
+  downloadEquipTemplate
+} from './apis/index'
 export default {
   components: {
     searchCondition,
     netDeviceDetails,
     netDeviceEdit,
-    netDeviceHistory
+    netDeviceHistory,
+    ConfigFileUpload
   },
   data () {
     return {
@@ -87,6 +89,7 @@ export default {
       dialogEditVisible: false,
       dialogSynDataVisible: false,
       dialogHistoryVisible: false,
+      dialogUploadVisible: false,
       addressData: [],
       netDeviceListData: undefined,
       netDeviceDetails: undefined,
@@ -95,53 +98,58 @@ export default {
       syncDataStatus: '',
       maxlength: 30,
       loading: true,
+      uploadFlag: 'ne',
       searchConditionList: {
-        'city': '',
-        'condition': '',
-        'currentPage': 1,
-        'district': '',
-        'pageSize': 10,
-        'province': ''
-      },
-      defaultProps: {
-        label: 'name',
-        children: 'children',
-        value: 'name'
+        city: '',
+        condition: '',
+        currentPage: 1,
+        district: '',
+        pageSize: 10,
+        province: '',
+        pageFlag: 'ne'
       },
       tableTitleList: [
         {
           colName: '省（直辖市）',
           prop: 'courtDto.province',
           width: 120
-        }, {
+        },
+        {
           colName: '市',
           prop: 'courtDto.city',
           width: 120
-        }, {
+        },
+        {
           colName: '区',
           prop: 'courtDto.district',
           width: 120
-        }, {
+        },
+        {
           colName: '小区名称',
           prop: 'courtDto.name',
           width: 120
-        }, {
+        },
+        {
           colName: '设备名称',
           prop: 'name',
           width: 120
-        }, {
+        },
+        {
           colName: '设备类型/型号',
           prop: 'type',
           width: 120
-        }, {
+        },
+        {
           colName: '管理IP',
           prop: 'mgmtIp',
           width: 120
-        }, {
+        },
+        {
           colName: '运行状态',
           prop: 'status',
           width: 150
-        }, {
+        },
+        {
           colName: '描述',
           prop: 'remark'
         }
@@ -149,31 +157,61 @@ export default {
       detailsTitle: '查看详情',
       editTitle: '编辑',
       refreshTitle: '比对更新',
-      historyTitle: '历史信息',
-      details: require('./assets/images/details.png'),
-      edit: require('./assets/images/edit.png'),
-      refresh: require('./assets/images/refresh.png'),
-      history: require('./assets/images/history.png')
+      historyTitle: '历史信息'
     }
   },
   methods: {
     // 查询
-    _handleFilter () {
-      console.log('_handleFilter --- > ' + JSON.stringify(this.searchConditionList))
-      this.loading = true
-      getNetDeviceInfoByPage(this.searchConditionList)
-        .then(
-          function (result) {
-            this.netDeviceListData = result.netequipList
-            this.total = result.pageCount
-            this.loading = false
-          }.bind(this)
-        )
-        .catch(
-          function (error) {
+    _handleFilter (params, type) {
+      delete params.pageFlag
+      console.log('_handleFilter --- > ' + JSON.stringify(params))
+      if (type === 'search') {
+        this.loading = true
+        getNetDeviceInfoByPage(this.params)
+          .then(
+            function (result) {
+              this.netDeviceListData = result.netequipList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(function (error) {
             console.log(error)
-          }
-        )
+          })
+      } else if (type === 'download') {
+        downloadResultFile(params)
+          .then(
+            function (result) {
+              this.auServerListData = result.auServersList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(
+            function (error) {
+              this.loading = false
+              console.log(error)
+            }.bind(this)
+          )
+      } else if (type === 'upload') {
+        this.dialogStatus = '上传网络设备配置信息'
+        this.dialogUploadVisible = true
+      } else if (type === 'downtemplate') {
+        downloadEquipTemplate(params)
+          .then(
+            function (result) {
+              this.auServerListData = result.auServersList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(
+            function (error) {
+              this.loading = false
+              console.log(error)
+            }.bind(this)
+          )
+      }
     },
 
     // 查看网络设备每条详细信息
@@ -183,14 +221,14 @@ export default {
       var eachRowUUID = rowData.uuid
       console.log('check rowData -- >' + eachRowUUID)
       getNetDeviceDetails(eachRowUUID)
-          .then(
-            function (result) {
-              console.log(result)
-              this.netDeviceDetails = result.auNetequip
-              this.dialogDetailsVisible = true
-            }.bind(this)
-          )
-          .catch()
+        .then(
+          function (result) {
+            console.log(result)
+            this.netDeviceDetails = result.auNetequip
+            this.dialogDetailsVisible = true
+          }.bind(this)
+        )
+        .catch()
     },
 
     // 编辑每条网络设备信息
@@ -200,18 +238,18 @@ export default {
       var eachRowUUID = rowData.uuid
       console.log('edit rowData -- >' + eachRowUUID)
       getNetDeviceDetails(eachRowUUID)
-          .then(
-            function (result) {
-              this.netDeviceDetails = result.auNetequip
-              console.log('edit net details --- > ' + JSON.stringify(this.netDeviceDetails))
-              this.dialogEditVisible = true
-            }.bind(this)
-          )
-          .catch(
-            function (error) {
-              console.log(error)
-            }
-          )
+        .then(
+          function (result) {
+            this.netDeviceDetails = result.auNetequip
+            console.log(
+              'edit net details --- > ' + JSON.stringify(this.netDeviceDetails)
+            )
+            this.dialogEditVisible = true
+          }.bind(this)
+        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
 
     // 更新网络设备的信息
@@ -227,23 +265,19 @@ export default {
                 type: 'success',
                 duration: 2000
               })
-            // 再次加载列表的数据
+              // 加载数据
               this.loadData()
             } else {
               this.$message({
-                title: '更新失败',
                 message: '保存失败',
-                type: 'success',
-                duration: 2000
+                type: 'success'
               })
             }
           }.bind(this)
         )
-        .catch(
-          function (error) {
-            console.log(error)
-          }
-        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
 
     // 刷新网络设备信息
@@ -255,42 +289,23 @@ export default {
       syncNetDeviceData(eachRowUUID)
         .then(
           function (result) {
-            console.log(this.syncDataStatus = result.syncMessage.msg)
+            console.log((this.syncDataStatus = result.syncMessage.msg))
             this.syncDataStatus = result.syncMessage.msg
             if (this.syncDataStatus) {
               this.synDataLoading = false
-              // setTimeout(() => {
-              // }, 12000)
               this.$message({
-                title: '数据更新成功',
-                message: '数据更新成功',
-                type: 'success',
-                duration: 2000
+                message: '保存成功',
+                type: 'success'
               })
-              // 再次加载列表的数据
+              // 加载数据
               this.loadData()
-            } else {
-              this.$message({
-                title: '更新失败',
-                message: '数据更新失败',
-                type: 'error',
-                duration: 2000
-              })
             }
           }.bind(this)
         )
-        .catch(
-          function (error) {
-            this.synDataLoading = false
-            console.log(error)
-            this.$message({
-              title: '更新失败',
-              message: '数据更新失败',
-              type: 'error',
-              duration: 2000
-            })
-          }
-        )
+        .catch(function (error) {
+          this.synDataLoading = false
+          console.log(error)
+        })
     },
 
     // 查看网络设备信息的历史记录
@@ -300,20 +315,19 @@ export default {
       var eachRowUUID = rowData.uuid
       console.log('history rowData -- >' + eachRowUUID)
       getauServersHistoryList(eachRowUUID)
-          .then(
-            function (result) {
-              this.netDeviceHistoryData = result.auNetequipHisList
-              this.dialogHistoryVisible = true
-            }.bind(this)
-          )
-          .catch(
-            function (error) {
-              console.log(error)
-            }
-          )
+        .then(
+          function (result) {
+            this.netDeviceHistoryData = result.auNetequipHisList
+            this.dialogHistoryVisible = true
+          }.bind(this)
+        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
     // 初始加载网络设备的信息
     loadData () {
+      delete this.searchConditionList.pageFlag
       getNetDeviceInfoByPage(this.searchConditionList)
         .then(
           function (result) {
@@ -323,11 +337,9 @@ export default {
             this.loading = false
           }.bind(this)
         )
-        .catch(
-          function (error) {
-            console.log(error)
-          }
-        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
 
     // 改变分页大小
@@ -340,6 +352,13 @@ export default {
     handleCurrentChange (val) {
       this.searchConditionList.page = val
       this.loadData()
+    },
+    _handleCloseUploadDialog () {
+      this.dialogUploadVisible = false
+    },
+    _handleBeforClose () {
+      this.dialogUploadVisible = false
+      this.$refs.uploadCmpt.clearFileList()
     }
   },
   mounted () {
@@ -349,5 +368,5 @@ export default {
 </script>
 
 <style scoped>
-@import "assets/css/upgrademgmt.less"
+@import 'assets/css/upgrademgmt.less';
 </style>
--- src/views/ApplicationUpgrade/ConfigurationMgmt/ServerHardwareCfgInfo.vue
+++ src/views/ApplicationUpgrade/ConfigurationMgmt/ServerHardwareCfgInfo.vue
@@ -14,15 +14,12 @@
             <el-table-column fixed="right" label="操作" width="200">
               <template slot-scope="scope">
                 <el-button @click="_handleCheckDetails(scope.$index)" type="text" :title="detailsTitle" class="el-icon-view" style="font-size:15px;color: #0078f4">
-                  <!-- <img :src="details"/> -->
                   </el-button>
                 <el-button @click="_handleEdit(scope.$index)" type="text" :title="editTitle" class="el-icon-edit"  style="font-size:15px;color: #0078f4">
                 </el-button>
                 <el-button @click="_handleSynData(scope.$index)" type="text" :title="refreshTitle" class="el-icon-refresh" style="font-size:15px;color: #0078f4">
-                  <!-- <img :src="refresh"/> -->
                 </el-button>
                 <el-button @click="_handleCheckHistory(scope.$index)" type="text" :title="historyTitle" class="el-icon-time" style="font-size:15px;color: #0078f4">
-                  <!-- <img :src="history"/> -->
                   </el-button>
               </template>
             </el-table-column>
@@ -43,21 +40,18 @@
         </div>
       </el-col>
     </el-row>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogDetailsVisible" top="8vh">
-        <server-hardware-details :auServerDetails="auServerDetails"></server-hardware-details >
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogEditVisible" top="8vh">
-        <server-hardware-edit :auServerDetails="auServerDetails" @saveServInfoEvent="_updateServerInfo"></server-hardware-edit>
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog :title="dialogStatus" :visible.sync="dialogHistoryVisible" top="8vh" width="80%">
-        <server-hardware-history :auServerHistory="auServerHistory"></server-hardware-history>
-      </el-dialog>
-    </div>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogDetailsVisible" top="8vh">
+      <server-hardware-details :auServerDetails="auServerDetails"></server-hardware-details >
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogEditVisible" top="8vh">
+      <server-hardware-edit :auServerDetails="auServerDetails" @saveServInfoEvent="_updateServerInfo"></server-hardware-edit>
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogHistoryVisible" top="8vh" width="80%">
+      <server-hardware-history :auServerHistory="auServerHistory"></server-hardware-history>
+    </el-dialog>
+    <el-dialog :title="dialogStatus" :visible.sync="dialogUploadVisible" width="40%" :before-close="_handleBeforClose">
+      <config-file-upload ref="uploadCmpt" :uploadFlag="uploadFlag" @handleUploadSuccessEvent="_handleCloseUploadDialog"></config-file-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -66,14 +60,24 @@ import searchCondition from './components/SearchCondition'
 import serverHardwareDetails from './components/ServerHardwareDetails'
 import serverHardwareEdit from './components/ServerHardwareEdit'
 import serverHardwareHistory from './components/ServerHardwareHistory'
+import ConfigFileUpload from './components/ConfigFileUpload'
 
-import { getauServersDetails, getauServersByPage, updateAuServerInfor, getauServersHistoryList, syncauServersData } from './apis/index'
+import {
+  getauServersDetails,
+  getauServersByPage,
+  updateAuServerInfor,
+  getauServersHistoryList,
+  syncauServersData,
+  downloadResultFile,
+  downHardwareTemplate
+} from './apis/index'
 export default {
   components: {
     searchCondition,
     serverHardwareDetails,
     serverHardwareEdit,
-    serverHardwareHistory
+    serverHardwareHistory,
+    ConfigFileUpload
   },
   data () {
     return {
@@ -86,6 +90,7 @@ export default {
       dialogEditVisible: false,
       dialogSynDataVisible: false,
       dialogHistoryVisible: false,
+      dialogUploadVisible: false,
       auServerListData: undefined,
       auServerDetails: undefined,
       auServerHistory: undefined,
@@ -93,53 +98,58 @@ export default {
       syncDataStatus: '',
       maxlength: 30,
       loading: true,
+      uploadFlag: 'hw',
       searchConditionList: {
-        'city': '',
-        'condition': '',
-        'currentPage': 1,
-        'district': '',
-        'pageSize': 10,
-        'province': ''
-      },
-      defaultProps: {
-        label: 'name',
-        value: 'name',
-        children: 'children'
+        city: '',
+        condition: '',
+        currentPage: 1,
+        district: '',
+        pageSize: 10,
+        province: '',
+        pageFlag: 'hw'
       },
       tableTitleList: [
         {
           colName: '省（直辖市）',
           prop: 'courtDto.province',
           width: 120
-        }, {
+        },
+        {
           colName: '市',
           prop: 'courtDto.city',
           width: 100
-        }, {
+        },
+        {
           colName: '区',
           prop: 'courtDto.district',
           width: 100
-        }, {
+        },
+        {
           colName: '小区名称',
           prop: 'courtDto.name',
           width: 120
-        }, {
+        },
+        {
           colName: '服务器产品名称',
           prop: 'name',
           width: 200
-        }, {
+        },
+        {
           colName: '服务器SN',
           prop: 'serialNo',
           width: 120
-        }, {
+        },
+        {
           colName: '服务器厂商',
           prop: 'vendor',
           width: 120
-        }, {
+        },
+        {
           colName: '服务器类型/型号',
           prop: 'model',
           width: 340
-        }, {
+        },
+        {
           colName: '描述',
           prop: 'remark'
         }
@@ -147,37 +157,64 @@ export default {
       detailsTitle: '查看详情',
       editTitle: '编辑',
       refreshTitle: '比对更新',
-      historyTitle: '历史信息',
-      details: require('./assets/images/details.png'),
-      edit: require('./assets/images/edit.png'),
-      refresh: require('./assets/images/refresh.png'),
-      history: require('./assets/images/history.png')
+      historyTitle: '历史信息'
     }
   },
   methods: {
     // 查询
-    _handleFilter (params) {
-      this.loading = true
-      getauServersByPage(params)
-        .then(
-          function (result) {
-            this.auServerListData = result.auServersList
-            this.total = result.pageCount
-            this.loading = false
-          }.bind(this)
-        ).catch(
-          function (error) {
-            this.$message({
-              message: '查询失败',
-              center: true,
-              showClose: true,
-              type: 'error',
-              duration: 2000
-            })
-            this.loading = false
-            console.log(error)
-          }.bind(this)
-        )
+    _handleFilter (params, type) {
+      delete params.pageFlag
+      if (type === 'search') {
+        this.loading = true
+        getauServersByPage(params)
+          .then(
+            function (result) {
+              this.auServerListData = result.auServersList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(
+            function (error) {
+              this.loading = false
+              console.log(error)
+            }.bind(this)
+          )
+      } else if (type === 'download') {
+        // this.loading = false
+        downloadResultFile(params)
+          .then(
+            function (result) {
+              this.auServerListData = result.auServersList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(
+            function (error) {
+              this.loading = false
+              console.log(error)
+            }.bind(this)
+          )
+      } else if (type === 'upload') {
+        this.dialogStatus = '上传硬件服务器配置信息'
+        this.dialogUploadVisible = true
+      } else if (type === 'downtemplate') {
+        downHardwareTemplate(params)
+          .then(
+            function (result) {
+              this.auServerListData = result.auServersList
+              this.total = result.pageCount
+              this.loading = false
+            }.bind(this)
+          )
+          .catch(
+            function (error) {
+              this.loading = false
+              console.log(error)
+            }.bind(this)
+          )
+      }
     },
 
     // 查看服务器每条详细信息
@@ -187,12 +224,13 @@ export default {
       var eachRowUUID = rowData.uuid
       console.log('check rowData -- >' + eachRowUUID)
       getauServersDetails(eachRowUUID)
-          .then(
-            function (result) {
-              this.auServerDetails = result.auServers
-              this.dialogDetailsVisible = true
-            }.bind(this)
-          ).catch()
+        .then(
+          function (result) {
+            this.auServerDetails = result.auServers
+            this.dialogDetailsVisible = true
+          }.bind(this)
+        )
+        .catch()
     },
 
     // 编辑每条服务器信息
@@ -201,19 +239,20 @@ export default {
       var rowData = this.auServerListData[rowIdx]
       var eachRowUUID = rowData.uuid
       getauServersDetails(eachRowUUID)
-          .then(
-            function (result) {
-              this.auServerDetails = result.auServers
-              this.dialogEditVisible = true
-              console.log('edit server details ----------->   ' + JSON.stringify(this.auServerDetails))
-            }.bind(this)
-          ).catch(
-            function (error) {
-              console.log(error)
-            }
-          )
+        .then(
+          function (result) {
+            this.auServerDetails = result.auServers
+            this.dialogEditVisible = true
+            console.log(
+              'edit server details ----------->   ' +
+                JSON.stringify(this.auServerDetails)
+            )
+          }.bind(this)
+        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
-
     // 更新server的信息
     _updateServerInfo (params) {
       updateAuServerInfor(params)
@@ -231,13 +270,10 @@ export default {
             }
           }.bind(this)
         )
-        .catch(
-          function (error) {
-            console.log(error)
-          }
-        )
+        .catch(function (error) {
+          console.log(error)
+        })
     },
-
     // 刷新服务器信息
     _handleSynData (rowIdx) {
       this.synDataLoading = true
@@ -256,46 +292,39 @@ export default {
                 message: '数据更新成功',
                 type: 'success'
               })
-              // 再次加载列表的数据
+              // 再次加载数据
               this.loadData()
             }
           }.bind(this)
-        ).catch(
-          function (error) {
-            this.synDataLoading = false
-            console.log(error)
-          }
         )
+        .catch(function (error) {
+          this.synDataLoading = false
+          console.log(error)
+        })
     },
-
-    // 查看服务器信息的历史记录
+    // 服务器信息的历史记录
     _handleCheckHistory (rowIdx) {
       this.dialogStatus = '服务器硬件历史信息详情'
       var rowData = this.auServerListData[rowIdx]
       var eachRowUUID = rowData.uuid
       console.log('history rowData -- >' + eachRowUUID)
       getauServersHistoryList(eachRowUUID)
-          .then(
-            function (result) {
-              this.auServerHistory = result.auServersHisList
-              this.dialogHistoryVisible = true
-            }.bind(this)
-          )
-          .catch(
-            function (error) {
-              this.$message({
-                message: '查询失败',
-                type: 'error',
-                duration: 2000
-              })
-              console.log(error)
-              this.loading = false
-            }.bind(this)
-          )
+        .then(
+          function (result) {
+            this.auServerHistory = result.auServersHisList
+            this.dialogHistoryVisible = true
+          }.bind(this)
+        )
+        .catch(
+          function (error) {
+            console.log(error)
+            this.loading = false
+          }.bind(this)
+        )
     },
-
     // 初始加载服务器的信息
     loadData () {
+      delete this.searchConditionList.pageFlag
       getauServersByPage(this.searchConditionList)
         .then(
           function (result) {
@@ -304,24 +333,30 @@ export default {
             this.total = result.pageCount
             this.loading = false
           }.bind(this)
-        ).catch(
+        )
+        .catch(
           function (error) {
             this.loading = false
             console.log(error)
-          }
+          }.bind(this)
         )
     },
-
     // 改变分页大小
     handleSizeChange (val) {
       this.searchConditionList.pageSize = val
       this.loadData()
     },
-
     // 跳转页数
     handleCurrentChange (val) {
       this.searchConditionList.page = val
       this.loadData()
+    },
+    _handleCloseUploadDialog () {
+      this.dialogUploadVisible = false
+    },
+    _handleBeforClose () {
+      this.dialogUploadVisible = false
+      this.$refs.uploadCmpt.clearFileList()
     }
   },
   mounted () {
@@ -331,5 +366,5 @@ export default {
 </script>
 
 <style scoped>
- @import "assets/css/upgrademgmt.less"
+@import 'assets/css/upgrademgmt.less';
 </style>
