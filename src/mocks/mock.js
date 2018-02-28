984186a37a647650601ac3c881f773573540be53
qiyongping <qiyongping@hdsc.com>
2018-02-28 11:28:00 +0800

    move source from mdmmgnt to mdmapp

--- src/mocks/mock.js
+++ src/mocks/mock.js
@@ -5,7 +5,6 @@
 // 引用子模块的mock文件
 import '@/views/demo/mocks/mock.js'
 import '@/views/UserMgmt/login/mocks/mock.js'
-import '@/views/MdmMgmt/mocks/mock.js'
 import '@/views/MapAnalysisApp/mocks/mock.js'
 import '@/views/HouseAllApp/mocks/mock.js'
 import '@/views/MdmApp/mocks/mock.js'
