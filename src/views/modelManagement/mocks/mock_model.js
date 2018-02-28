4f4826244661ed03d505c3a3088ef93474423188
chenhaiyun <chenhaiyun>
2018-02-28 10:14:13 +0800

    remove mock data
    remove mock data

--- src/views/modelManagement/mocks/mock_model.js
+++ src/views/modelManagement/mocks/mock_model.js
@@ -10,18 +10,7 @@ Mock.mock(MOCK_API_URL + '/model/list', function () {
     'data': {
       'total': 19,
       'pageSize': 1000,
-      'list': [
-        {'id': 1, 'name': '模型名称001', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 2, 'name': '模型名称002', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 3, 'name': '模型名称003', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 4, 'name': '模型名称004', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 5, 'name': '模型名称005', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 6, 'name': '模型名称006', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 7, 'name': '模型名称007', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 8, 'name': '模型名称008', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 9, 'name': '模型名称009', 'address': '这个是模型的描述信息', 'date': '2016-05-02'},
-        {'id': 10, 'name': '模型名称010', 'address': '这个是模型的描述信息', 'date': '2016-05-02'}
-      ],
+      'list': [],
       'currentPage': 1
     }
   }
@@ -48,56 +37,7 @@ Mock.mock(MOCK_API_URL + '/getModelList', function () {
     'data': {
       'total': 19,
       'pageCount': 1000,
-      'items': [
-        {
-          'id': 1,
-          'name': '节点类型1',
-          'description': '节点版本1.0.0',
-          'nodeName': '节点12306',
-          'runtimeType': '否',
-          'ownUser': 'Karl',
-          'createUser': 'Karl',
-          'latestVersion': '1.0.0',
-          'createTime': '2016-05-02 12:34:56',
-          'updateTime': '2016-05-22 12:34:56'
-        },
-        {
-          'id': 1,
-          'name': '节点类型1',
-          'description': '节点版本1.0.0',
-          'nodeName': '节点12306',
-          'runtimeType': '否',
-          'ownUser': 'Karl',
-          'createUser': 'Karl',
-          'latestVersion': '1.0.0',
-          'createTime': '2016-05-02 12:34:56',
-          'updateTime': '2016-05-22 12:34:56'
-        },
-        {
-          'id': 1,
-          'name': '节点类型1',
-          'description': '节点版本1.0.0',
-          'nodeName': '节点12306',
-          'runtimeType': '否',
-          'ownUser': 'Karl',
-          'createUser': 'Karl',
-          'latestVersion': '1.0.0',
-          'createTime': '2016-05-02 12:34:56',
-          'updateTime': '2016-05-22 12:34:56'
-        },
-        {
-          'id': 1,
-          'name': '节点类型1',
-          'description': '节点版本1.0.0',
-          'nodeName': '节点12306',
-          'runtimeType': '否',
-          'ownUser': 'Karl',
-          'createUser': 'Karl',
-          'latestVersion': '1.0.0',
-          'createTime': '2016-05-02 12:34:56',
-          'updateTime': '2016-05-22 12:34:56'
-        }
-      ],
+      'items': [],
       'currentPage': 1
     }
   }
