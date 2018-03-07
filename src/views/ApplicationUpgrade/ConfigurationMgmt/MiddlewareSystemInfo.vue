de0dd3c56e469a1d719986b3bbd46d4d9c07a2c2
wuchun <wuchun@hdsc.com>
2018-03-07 11:47:04 +0800

    更新升级 添加所有页面序号自动增加

--- src/views/ApplicationUpgrade/ConfigurationMgmt/MiddlewareSystemInfo.vue
+++ src/views/ApplicationUpgrade/ConfigurationMgmt/MiddlewareSystemInfo.vue
@@ -6,7 +6,7 @@
     <div v-loading="synDataLoading" element-loading-background="rgba(0, 0, 0, 0.8)" element-loading-text="玩命同步中...">
         <div style="margin-top: 15px">
           <el-table :data="middlewareListData" stripe border v-loading="loading" height="680">
-            <el-table-column type="index" label="序号" width="50">
+            <el-table-column type="index" :index="indexMethod" label="序号" width="50">
             </el-table-column>
             <el-table-column v-for="(item, index) in tableTitleList " :key="index" :prop="item.prop" :label="item.colName" sortable>
             </el-table-column>
@@ -331,6 +331,14 @@ export default {
     handleCurrentChange (val) {
       this.searchConditionList.currentPage = val
       this.loadData()
+    },
+    indexMethod (index) {
+      var pageSize = this.searchConditionList.pageSize
+      if (!pageSize) pageSize = 10
+      var page = this.searchConditionList.currentPage
+      if (!page) page = 1
+      var computedIndex = pageSize * (page - 1) + index + 1
+      return computedIndex
     }
   },
   mounted () {
