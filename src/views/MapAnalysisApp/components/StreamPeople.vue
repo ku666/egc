5bfed038f6906a475c932ff9d8940a94ac36936f
dengdaichao <dengdaichao@hdsc.con>
2018-03-09 14:19:29 +0800

    人员

--- src/views/MapAnalysisApp/components/StreamPeople.vue
+++ src/views/MapAnalysisApp/components/StreamPeople.vue
@@ -187,14 +187,8 @@ export default {
       this.myChart = this.$echarts.init(this.myChartNode)
       this.myChart.setOption(this.echartsData())
       // 判断时间是否选择正确
-      if (this.isRequest) {
-        this.getPerData()
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请选择正确的时间'
-        })
-      }
+      this.onTimeJudgment()
+      if (this.isRequest) this.getPerData()
       // 多次点击
       if (this.chartClickNum > 0) return
       this.chartClickNum++
@@ -386,14 +380,8 @@ export default {
       let elTable = document.querySelector('.el-table')
       elTable.style.height = '382px'
       // 判断时间是否选择正确
-      if (this.isRequest) {
-        this.getPgingData()
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请选择正确的时间'
-        })
-      }
+      this.onTimeJudgment()
+      if (this.isRequest) this.getPgingData()
       // 多次点击
       if (this.tableClickNum > 0) return
       this.tableClickNum++
@@ -426,11 +414,6 @@ export default {
       if (this.isRequest) {
         if (this.isTableShow) this.getPgingData()
         else this.getPerData()
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请选择正确的时间'
-        })
       }
       // 重置点击次数
       if (this.isTableShow) {
@@ -478,14 +461,8 @@ export default {
     onSizeChange: function (val) {
       this.parameter.pageSize = val
       this.parameter.currentPage = 1
-      if (this.isRequest) {
-        this.getPgingData()
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请选择正确的时间'
-        })
-      }
+      this.onTimeJudgment()
+      if (this.isRequest) this.getPgingData()
     },
     /**
     * @description 分页组件当前页变化 *
@@ -493,14 +470,8 @@ export default {
     */
     onCurrentChange: function (val) {
       this.parameter.currentPage = val
-      if (this.isRequest) {
-        this.getPgingData()
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请选择正确的时间'
-        })
-      }
+      this.onTimeJudgment()
+      if (this.isRequest) this.getPgingData()
     },
     /**
     * @description 获取人流信息(图表) *
